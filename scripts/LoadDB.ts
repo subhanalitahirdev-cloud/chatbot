4312be0b4a152631bdc0fe6ab178d8c4456966b3
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-05 17:24:19 +0500

    added langchain text splitter

--- scripts/LoadDB.ts
+++ scripts/LoadDB.ts
@@ -1,1 +1,4 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
+import { OpenAI } from "openai/client.js";
+import "dotenv/config";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
