148a053d47604cd5345e5d0f3c38b9bd0851b0c1
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-05 18:00:17 +0500

    Added Puppeteer and other packages

--- scripts/LoadDB.ts
+++ scripts/LoadDB.ts
@@ -2,3 +2,5 @@ import { DataAPIClient } from "@datastax/astra-db-ts";
 import { OpenAI } from "openai/client.js";
 import "dotenv/config";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import "dotenv/config";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
