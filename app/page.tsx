e30aae9f6c1a13799553424e065a34ba26ab49ed
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-06 17:34:04 +0500

    Build the page.tsx and added lottie animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
 import {useChat} from '@ai-sdk/react';
-import {Message} from 'ai';
+import Lottie from 'lottie-react';
+import animationData from '../public/lottie/Bot2.json'
+import { IoMdSend } from "react-icons/io";
+// import {Message} from 'ai';
 const Home = () => {
   const noMessage = true;
-  const {input,append,isLoading,messages,handleInputChange,handleSubmit} = useChat();
+  // const {input,append,isLoading,messages,handleInputChange,handleSubmit} = useChat();
   // function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
   //   setInput(event.target.value);
   // }
@@ -16,26 +20,30 @@ const Home = () => {
   //   setInput('');
   // }
   return (
-    <div className='chatbot-section'>
-      <Image
-        src="https://sbxxykwqkbzufkjtdtfp.supabase.co/storage/v1/object/public/portfolio-images/Upvave_Logo_Complete-removebg-preview.png"
-        alt="Upvave Logo"
-        width={200}
-        height={200}
-        className={noMessage ? '' : 'populated'}
-      />
-      <section>
-        {noMessage?(<>
-        <p className='starter-text'>At Upvave, we are on a mission to Master the Web Solutions. As a dynamic startup based in Lahore, we specialize in providing cutting-edge web solutions that help businesses and individuals thrive in the digital world. While our team has traditionally operated remotely, we are now transitioning to on-site operations, fostering a collaborative environment that drives innovation</p>
-        <br/>
-        {/*<PromptSuggestion /> */}
-        </>):(<>
-        {/*Show bubble */}
-        {/*<loading/> */}
-        <input type="text" className='question-box' onChange={handleInputChange} onSubmit={handleSubmit} value={input} placeholder='Write something...' />
-        </>)}
-
-      </section>
+    <div className='w-full h-screen bg-white flex justify-center items-center'>
+      <div className='w-[20rem] h-100 bg-black rounded-xl shadow-md p-5 flex flex-col justify-start items-center'>
+        {/* <Image
+          src="https://sbxxykwqkbzufkjtdtfp.supabase.co/storage/v1/object/public/portfolio-images/Upvave_Logo_Complete-removebg-preview.png"
+          alt="Upvave Logo"
+          width={100}
+          height={100}
+          className={noMessage ? '' : 'populated'}
+        /> */}
+          {noMessage?(<>
+          <Lottie animationData={animationData} loop={true} style={{width: '150px',height: '150px'}} />
+          <p className='text-center'>You can now get any informaiton related to Upvave on this chat bot.</p>
+          <br/>
+          {/*<PromptSuggestion /> */}
+          </>):(<>
+          {/*Show bubble */}
+          {/*<loading/> */}
+          {/* <input type="text" className='question-box' onChange={handleInputChange} onSubmit={handleSubmit} value={input} placeholder='Write something...' /> */}
+          </>)}
+          <div className='relative top-10 flex gap-2'>
+            <input type="text" placeholder='Write Something...' className='p-2 w-[80%] h-auto outline-0 border-2 border-white rounded-xl' />
+            <button className='p-2 bg-white rounded font-4xl cursor-pointer w-10 flex justify-center items-center text-black'><IoMdSend/></button>
+          </div>
+      </div>
     </div>
   )
 }
