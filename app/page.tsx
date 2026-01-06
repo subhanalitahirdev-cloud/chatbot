1c0ced3f8ce9f3c68b008c3351d3341351935b54
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-06 18:34:11 +0500

    Added loader and updated page.tsx and also  Commented Database Loading

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Image from 'next/image'
 import {useChat} from '@ai-sdk/react';
 import Lottie from 'lottie-react';
 import animationData from '../public/lottie/Bot2.json'
+import loaderData from '../public/lottie/Loader.json'
 import { IoMdSend } from "react-icons/io";
 // import {Message} from 'ai';
 const Home = () => {
   const noMessage = true;
+  const [submitting,setSubmitting] = React.useState(false);
   // const {input,append,isLoading,messages,handleInputChange,handleSubmit} = useChat();
   // function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
   //   setInput(event.target.value);
@@ -19,29 +21,41 @@ const Home = () => {
   //   // For now, just clear the input
   //   setInput('');
   // }
+  const recommedations = [
+    "What is Upvave?",
+    "What services does Upvave offer?",
+    "How can Upvave help my business grow?",
+    "Tell me about Upvave's web development process.",
+  ]
   return (
     <div className='w-full h-screen bg-white flex justify-center items-center'>
-      <div className='w-[20rem] h-100 bg-black rounded-xl shadow-md p-5 flex flex-col justify-start items-center'>
-        {/* <Image
-          src="https://sbxxykwqkbzufkjtdtfp.supabase.co/storage/v1/object/public/portfolio-images/Upvave_Logo_Complete-removebg-preview.png"
-          alt="Upvave Logo"
-          width={100}
-          height={100}
-          className={noMessage ? '' : 'populated'}
-        /> */}
-          {noMessage?(<>
-          <Lottie animationData={animationData} loop={true} style={{width: '150px',height: '150px'}} />
-          <p className='text-center'>You can now get any informaiton related to Upvave on this chat bot.</p>
-          <br/>
-          {/*<PromptSuggestion /> */}
-          </>):(<>
-          {/*Show bubble */}
-          {/*<loading/> */}
-          {/* <input type="text" className='question-box' onChange={handleInputChange} onSubmit={handleSubmit} value={input} placeholder='Write something...' /> */}
-          </>)}
-          <div className='relative top-10 flex gap-2'>
+      <div className='w-[25rem] h-100 bg-black rounded-xl shadow-md p-5 flex flex-col justify-start items-center'>
+          <div className='h-[80%] w-full flex justify-center flex-col overflow-hidden'>
+            {noMessage?(<>
+            <div className='w-full flex justify-center'>
+              <Lottie animationData={animationData} loop={true} style={{width: '150px',height: '150px'}} />
+            </div>
+            <p className='text-center font-[10px]'>You can now get any information related to Upvave on this chatbot.</p>
+            <div className='w-full m-2 h-auto flex flex-wrap'>
+              {recommedations.map((item, index) => (
+                <div key={index} className='text-[0.80rem] p-2 bg-white text-black rounded-xl m-1 cursor-pointer hover:bg-gray-200'>
+                  {item}
+                </div>
+              ))}
+            </div>
+            <br/>
+            {/*<PromptSuggestion /> */}
+            </>):(<>
+            {/*Show bubble */}
+            {/*<loading/> */}
+            {/* <input type="text" className='question-box' onChange={handleInputChange} onSubmit={handleSubmit} value={input} placeholder='Write something...' /> */}
+            </>)}
+          </div>
+          <div className='w-full flex gap-2'>
             <input type="text" placeholder='Write Something...' className='p-2 w-[80%] h-auto outline-0 border-2 border-white rounded-xl' />
-            <button className='p-2 bg-white rounded font-4xl cursor-pointer w-10 flex justify-center items-center text-black'><IoMdSend/></button>
+            <button onClick={()=>{
+              setSubmitting((prev)=>!prev);
+            }} className='p-2 bg-white rounded font-4xl cursor-pointer w-10 flex justify-center items-center text-black' >{submitting?<Lottie animationData={loaderData} loop={true}/>:<IoMdSend/>}</button>
           </div>
       </div>
     </div>
--- scripts/LoadDB.ts
+++ scripts/LoadDB.ts
@@ -1,84 +1,93 @@
-import { DataAPIClient } from "@datastax/astra-db-ts";
-import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import "dotenv/config";
-type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
-import OpenAI from "openai";
-const {
-  ASTRA_DB_API_ENDPOINT,
-  ASTRA_DB_API_KEY,
-  ASTRA_DB_COLLECTION,
-  ASTRA_DB_NAMESPACE,
-  OPENAI_API_KEY,
-} = process.env;
-const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
-const upvaveData = [
-  "https://upvave.com/",
-  "https://upvave.com/services/web-development",
-  "https://upvave.com/services/mobile-development",
-  "https://upvave.com/services/ui-ux-design",
-  "https://upvave.com/services/cloud-solutions",
-  "https://upvave.com/contact",
-  "https://upvave.com/about",
-];
-const client = new DataAPIClient(ASTRA_DB_API_KEY);
-const db = client.db(
-  typeof ASTRA_DB_API_ENDPOINT === "string" ? ASTRA_DB_API_ENDPOINT : "",
-  { keyspace: typeof ASTRA_DB_NAMESPACE === "string" ? ASTRA_DB_NAMESPACE : "" }
-);
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 512,
-  chunkOverlap: 100,
-});
-const createCollection = async (
-  similarityMetric: SimilarityMetric = "dot_product"
-) => {
-  const res = await db.createCollection(ASTRA_DB_COLLECTION || "upvavegpt", {
-    vector: {
-      dimension: 1536,
-      metric: similarityMetric,
-    },
-  });
-  console.log(res);
-};
-const loadSampleData = async () => {
-  const collection = await db.collection(ASTRA_DB_COLLECTION || "upvavegpt");
-  for await (const url of upvaveData) {
-    const content = await scrapePage(url);
-    const chunks = await splitter.splitText(
-      typeof content === "string" ? content : ""
-    );
-    for await (const chunk of chunks) {
-      const embedding = await openai.embeddings.create({
-        model: "text-embedding-3-small",
-        input: chunk,
-        encoding_format: "float",
-      });
-      const vector = embedding.data[0].embedding;
-      const res = await collection.insertOne({
-        $vector: vector,
-        text: chunk,
-      });
-      console.log(res);
-    }
-  }
-};
-const scrapePage = async (url: string) => {
-  const loader = new PuppeteerWebBaseLoader(url, {
-    launchOptions: {
-      headless: true,
-    },
-    gotoOptions: {
-      waitUntil: "domcontentloaded",
-    },
-    evaluate: async (page, browser) => {
-      const result = await page.evaluate(() => document.body.innerHTML);
-      await browser.close();
-      return result;
-    },
-  });
-  const scraped = await loader.scrape();
-  return scraped?.replace(/\n/g, " ");
-};
+/*
+---- IMPORTANT: UNCOMMIT THIS FILE WHEN YOU NEED TO STORE MORE DATA IN VECTOR AND STORE THEM IN DATASTAX ----
+This file has a depreciated dependency with the package name of "@langchain/community", which is currently not maintained and causes issues with the latest versions of other langchain packages. That is why that dependecy has been removed from the package.json file to avoid conflicts. If you need to change or add more data to the vector database, please uncommit this file, add the dependency "@langchain/community": "^1.1.2" back to the package.json file, run "npm seed" to add the data into the datastax database.
+To use this file, please follow these steps:    
+1- Uncommit this file from git.
+2- Add the dependency "@langchain/community": "^1.1.2" back to the package.json file.
+3- Run "npm seed" to add the data to database.
+*/
 
-createCollection().then(() => loadSampleData());
+// import { DataAPIClient } from "@datastax/astra-db-ts";
+// import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+// import "dotenv/config";
+// type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
+// import OpenAI from "openai";
+// const {
+//   ASTRA_DB_API_ENDPOINT,
+//   ASTRA_DB_API_KEY,
+//   ASTRA_DB_COLLECTION,
+//   ASTRA_DB_NAMESPACE,
+//   OPENAI_API_KEY,
+// } = process.env;
+// const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+// const upvaveData = [
+//   "https://upvave.com/",
+//   "https://upvave.com/services/web-development",
+//   "https://upvave.com/services/mobile-development",
+//   "https://upvave.com/services/ui-ux-design",
+//   "https://upvave.com/services/cloud-solutions",
+//   "https://upvave.com/contact",
+//   "https://upvave.com/about",
+// ];
+// const client = new DataAPIClient(ASTRA_DB_API_KEY);
+// const db = client.db(
+//   typeof ASTRA_DB_API_ENDPOINT === "string" ? ASTRA_DB_API_ENDPOINT : "",
+//   { keyspace: typeof ASTRA_DB_NAMESPACE === "string" ? ASTRA_DB_NAMESPACE : "" }
+// );
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 512,
+//   chunkOverlap: 100,
+// });
+// const createCollection = async (
+//   similarityMetric: SimilarityMetric = "dot_product"
+// ) => {
+//   const res = await db.createCollection(ASTRA_DB_COLLECTION || "upvavegpt", {
+//     vector: {
+//       dimension: 1536,
+//       metric: similarityMetric,
+//     },
+//   });
+//   console.log(res);
+// };
+// const loadSampleData = async () => {
+//   const collection = await db.collection(ASTRA_DB_COLLECTION || "upvavegpt");
+//   for await (const url of upvaveData) {
+//     const content = await scrapePage(url);
+//     const chunks = await splitter.splitText(
+//       typeof content === "string" ? content : ""
+//     );
+//     for await (const chunk of chunks) {
+//       const embedding = await openai.embeddings.create({
+//         model: "text-embedding-3-small",
+//         input: chunk,
+//         encoding_format: "float",
+//       });
+//       const vector = embedding.data[0].embedding;
+//       const res = await collection.insertOne({
+//         $vector: vector,
+//         text: chunk,
+//       });
+//       console.log(res);
+//     }
+//   }
+// };
+// const scrapePage = async (url: string) => {
+//   const loader = new PuppeteerWebBaseLoader(url, {
+//     launchOptions: {
+//       headless: true,
+//     },
+//     gotoOptions: {
+//       waitUntil: "domcontentloaded",
+//     },
+//     evaluate: async (page, browser) => {
+//       const result = await page.evaluate(() => document.body.innerHTML);
+//       await browser.close();
+//       return result;
+//     },
+//   });
+//   const scraped = await loader.scrape();
+//   return scraped?.replace(/\n/g, " ");
+// };
+
+// createCollection().then(() => loadSampleData());
