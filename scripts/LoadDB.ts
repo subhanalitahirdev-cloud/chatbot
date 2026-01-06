c59d4b380284c6de1cdc04b949be895a2ba8020d
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-06 12:01:05 +0500

    Made Collection and loading data functions

--- scripts/LoadDB.ts
+++ scripts/LoadDB.ts
@@ -1,10 +1,12 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import "dotenv/config";
+type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
 import OpenAI from "openai";
 const {
   ASTRA_DB_API_ENDPOINT,
   ASTRA_DB_API_KEY,
+  ASTRA_DB_COLLECTION,
   ASTRA_DB_NAMESPACE,
   OPENAI_API_KEY,
 } = process.env;
@@ -27,3 +29,27 @@ const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 512,
   chunkOverlap: 100,
 });
+const createCollection = async (
+  similarityMetric: SimilarityMetric = "dot_product"
+) => {
+  const res = await db.createCollection(ASTRA_DB_COLLECTION || "upvavegpt", {
+    vector: {
+      dimension: 1536,
+      metric: similarityMetric,
+    },
+  });
+};
+const loadSampleData = async () => {
+  const collection = await db.collection(ASTRA_DB_COLLECTION || "upvavegpt");
+  for await (const url of upvaveData) {
+    const content = await scrapePage(url);
+    const chunks = await splitter.splitText(content);
+    for await (const chunk of chunks) {
+      const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: chunk,
+        encoding_format: "float",
+      });
+    }
+  }
+};
