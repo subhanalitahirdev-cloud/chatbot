44c17e7f4d8b5c0026ac450df2e103be024dd9f3
subhanalitahirdev-cloud <subhanalitahir.dev@gmail.com>
2026-01-06 16:03:49 +0500

    updated page.tsx and global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,43 @@
-import Image from "next/image";
-
-export default function Home() {
+import React from 'react'
+import Image from 'next/image'
+import {useChat} from '@ai-sdk/react';
+import {Message} from 'ai';
+const Home = () => {
+  const noMessage = true;
+  const {input,append,isLoading,messages,handleInputChange,handleSubmit} = useChat();
+  // function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+  //   setInput(event.target.value);
+  // }
+  // function handleSubmit(event: React.FormEvent<HTMLInputElement>): void {
+  //   event.preventDefault();
+  //   if (input.trim() === '') return;
+  //   // Here you would typically send the input to the chat API or update state
+  //   // For now, just clear the input
+  //   setInput('');
+  // }
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='chatbot-section'>
+      <Image
+        src="https://sbxxykwqkbzufkjtdtfp.supabase.co/storage/v1/object/public/portfolio-images/Upvave_Logo_Complete-removebg-preview.png"
+        alt="Upvave Logo"
+        width={200}
+        height={200}
+        className={noMessage ? '' : 'populated'}
+      />
+      <section>
+        {noMessage?(<>
+        <p className='starter-text'>At Upvave, we are on a mission to Master the Web Solutions. As a dynamic startup based in Lahore, we specialize in providing cutting-edge web solutions that help businesses and individuals thrive in the digital world. While our team has traditionally operated remotely, we are now transitioning to on-site operations, fostering a collaborative environment that drives innovation</p>
+        <br/>
+        {/*<PromptSuggestion /> */}
+        </>):(<>
+        {/*Show bubble */}
+        {/*<loading/> */}
+        <input type="text" className='question-box' onChange={handleInputChange} onSubmit={handleSubmit} value={input} placeholder='Write something...' />
+        </>)}
+
+      </section>
     </div>
-  );
+  )
 }
+
+export default Home
